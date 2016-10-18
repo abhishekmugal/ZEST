ce8a5e42f0d777769127bdcdaba88321eae34fed
abhishekmugal <abhishek.mugal@gmail.com>
2016-10-18 23:07:12 +0900

    Add files via upload

---
+++ js/loading.js
@@ -0,0 +1,20 @@
+//Index file
+
+$(document).ready(function () { 
+	$('.section').hide().fadeIn(2000).delay(3000)
+});
+$(document).ready(function () { 
+	$('.date').hide().fadeIn(4500).delay(6000)
+});
+$(document).ready(function () { 
+	$('.date1').hide().fadeIn(4500).delay(6000)
+});
+$(document).ready(function () { 
+	$('.logo').hide().fadeIn(4500).delay(6000)
+});
+$(document).ready(function () { 
+	$('.clickreg').hide().fadeIn(6000).delay(7000)
+});
+$(document).ready(function () { 
+	$('.coep_logo1').hide().fadeIn(6000).delay(7000)
+});
